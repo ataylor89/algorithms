95c295088621b830496fab40fa01a3b3bf200804
ANDREW TAYLOR <rolyata47@gmail.com>
2020-01-19 19:09:18 -0600

    Committing addition.py

---
+++ addition.py
@@ -0,0 +1,24 @@
+# This code is contributed by 
+# Smitha Dinesh Semwal 
+# https://www.geeksforgeeks.org/add-two-numbers-without-using-arithmetic-operators/
+
+def Add(x, y): 
+  
+    # Iterate till there is no carry  
+    while (y != 0): 
+      
+        # carry now contains common 
+        # set bits of x and y 
+        carry = x & y 
+  
+        # Sum of bits of x and y where at 
+        # least one of the bits is not set 
+        x = x ^ y 
+  
+        # Carry is shifted by one so that    
+        # adding it to x gives the required sum 
+        y = carry << 1
+      
+    return x 
+  
+print(Add(15, 32))
